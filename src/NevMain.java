ec971d76c87c5970338f121889bac073978ecf1c
1 <vasilijnenastev@gmail.com>
2024-11-17 18:25:29 +0400

    HW132.2

--- src/NevMain.java
+++ src/NevMain.java
@@ -2,18 +2,23 @@ public class NevMain {
 
 
     public static void main(String[] args) {
-        Author author = new Author("Кинг ","Стивен" );
-        Book book = new Book("Оно",author,1986);
-        System.out.println("Название Книги - "+book.getTitle());
-        System.out.println("Автор книги - " + author.getSurname()+ author.getName());
-        System.out.println("Год выпуска книги " + book.getYear());
-        Author author1 = new Author("Дюма ","Александр");
-        Book book1 = new Book("Учитель фехтования",author1,1840);
-        System.out.println("Название Книги - "+book1.getTitle());
-        System.out.println("Автор книги - " + author1.getSurname()+author1.getName());
-        System.out.println("Год выпуска книги " + book1.getYear());
-        book1.setYear(1989);
-        System.out.println("Год перевыпуска книги " + book1.getYear());
+        Author king = new Author("Кинг ", "Стивен");
+        Book it = new Book("Оно", king, 1986);
+        System.out.println("Название Книги - " + it.getTitle());
+        System.out.println("Автор книги - " + king.getSurname() + king.getName());
+        System.out.println("Год выпуска книги " + it.getYear());
+        System.out.println(new Book("Оно", new Author("Стивен", "Кинг"), 1986));
+        System.out.println();
+        Author dumas = new Author("Дюма ", "Александр");
+        Book teacher = new Book("Учитель фехтования", dumas, 1840);
+        System.out.println("Название Книги - " + teacher.getTitle());
+        System.out.println("Автор книги - " + dumas.getSurname() + dumas.getName());
+        System.out.println("Год выпуска книги " + teacher.getYear());
+        System.out.println(new Book("Учитель фехтования", new Author("Александр", "Дюма"), 1840));
+        teacher.setYear(1989);
+        System.out.println("Год перевыпуска книги " + teacher.getYear());
+        System.out.println();
+        System.out.println("Сравнение книг: " + it.equals(teacher));
 
     }
 }
